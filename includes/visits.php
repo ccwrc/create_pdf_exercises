70b4cb8ac4998b31733011664bd0b914bcff90ac
ccwrc <j.muszelik@gmail.com>
2017-05-11 13:34:06 +0200

    add index + visit counter

--- includes/visits.php
+++ includes/visits.php
@@ -1,10 +1,11 @@
 <?php
 
 if (!isset($_COOKIE['visits'])) {
-    setcookie('visits', 1, time() + 3600 * 365);
-    echo "Witaj pierwszy raz na stronie<br>";
+    setcookie('visits', 1, time() + 3600 * 24 * 365); // rok waznosci
+    echo "Witaj pierwszy raz na stronie <br/>";
 } else {
     $visit = $_COOKIE['visits'];
-    echo "Witaj, odwiedziłeś nas już " . $visit++ . " razy!<br>";
-    setcookie('visits', $visit, time() + 3600 * 365);
+    echo "Witaj, odwiedziłeś nas już " . $visit++ . " razy! <br/>";
+    setcookie('visits', $visit, time() + 3600 * 24 * 365);
 }
+
