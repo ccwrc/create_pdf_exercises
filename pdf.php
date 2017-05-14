4d89478d30d189ded6c9f0bad16951ecdedd462b
ccwrc <j.muszelik@gmail.com>
2017-05-14 16:31:53 +0200

    calculation of flight time

--- pdf.php
+++ pdf.php
@@ -1,20 +1,39 @@
 <?php
 
+require_once './vendor/autoload.php';
+include 'includes/airports.php';
+
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    /* 
-  'departure' => string '1' (length=1)
-  'arrival' => string '4' (length=1)
-  'localDepartureTime' => string '11-11-2011 11:11:11' (length=19)
-  'length' => string '5' (length=1)
-  'price' => string '0.06' (length=4) */
+
     if (($_POST['departure'] != $_POST['arrival']) 
             && (DateTime::createFromFormat("d-m-Y H:i:s", $_POST['localDepartureTime'])) 
-            && ($_POST['price'] > 0) && ($_POST['length'] > 0)) {
-        echo "rózne";
-    }
+            && ($_POST['price'] > 0) && ($_POST['length'] > 0) && is_numeric($_POST['departure']) 
+            && is_numeric($_POST['arrival'])) {
+        /* 'name'     => 'Chicago Rockford International Airport',
+          'code'     => 'RFD',
+          'timezone' => 'America/Chicago', */
+        $departure = (int) $_POST['departure'];
+        $departureTimezone = new DateTimeZone($airports[$departure]['timezone']);
+        $arrival = (int) $_POST['arrival'];
+        $arrivalTimezone = new DateTimeZone($airports[$arrival]['timezone']);
 
-    
-    
-    
+        $localTime = $_POST['localDepartureTime'];
+        $flightTime = $_POST['length'];
+
+        $dateDeparture = new DateTime();
+        $dateDeparture->setTimezone($departureTimezone);
+        $dateDeparture->modify($localTime);
+        $dateDeparture->format('d.m.Y H:i:s');
+        var_dump($dateDeparture);
+        $dateArrival = new DateTime();
+        $dateArrival->setTimezone($arrivalTimezone);
+        $dateArrival->modify($localTime . "+" . $flightTime . "hours");
+        $dateArrival->format('d.m.Y H:i:s');
+        var_dump($dateArrival);
+        
+        
+        
+        
+    }
 }
 
